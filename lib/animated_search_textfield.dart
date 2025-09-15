986410581b9b844f246728345ddc1556faf4e561
Nilesh Senta <nileshsenta.dashstak@gmail.com>
2025-09-15 12:19:54 +0530

    initial commit

--- lib/animated_search_textfield.dart
+++ lib/animated_search_textfield.dart
@@ -150,29 +150,29 @@ class _AnimatedSearchTextFieldState extends State<AnimatedSearchTextField>
               /// Use custom FAB if provided, otherwise default toggle button
               widget.floatingActionButton != null
                   ? FloatingActionButton(
-                onPressed: toggle,
-                elevation: widget.floatingActionButton!.elevation,
-                backgroundColor:
-                widget.floatingActionButton!.backgroundColor ??
-                    Colors.deepOrange,
-                foregroundColor:
-                widget.floatingActionButton!.foregroundColor,
-                shape: widget.floatingActionButton!.shape,
-                child: widget.floatingActionButton!.child,
-              )
+                      onPressed: toggle,
+                      elevation: widget.floatingActionButton!.elevation,
+                      backgroundColor:
+                          widget.floatingActionButton!.backgroundColor ??
+                              Colors.deepOrange,
+                      foregroundColor:
+                          widget.floatingActionButton!.foregroundColor,
+                      shape: widget.floatingActionButton!.shape,
+                      child: widget.floatingActionButton!.child,
+                    )
                   : FloatingActionButton(
-                onPressed: toggle,
-                elevation: 0,
-                shape: CircleBorder(
-                    side: BorderSide(color: Colors.grey.shade300)),
-                backgroundColor:
-                widget.iconBackgroundColor ?? Colors.deepOrange,
-                child: Icon(
-                  expanded ? widget.backIcon : widget.searchIcon,
-                  size: widget.iconSize,
-                  color: widget.iconColor ?? Colors.white,
-                ),
-              ),
+                      onPressed: toggle,
+                      elevation: 0,
+                      shape: CircleBorder(
+                          side: BorderSide(color: Colors.grey.shade300)),
+                      backgroundColor:
+                          widget.iconBackgroundColor ?? Colors.deepOrange,
+                      child: Icon(
+                        expanded ? widget.backIcon : widget.searchIcon,
+                        size: widget.iconSize,
+                        color: widget.iconColor ?? Colors.white,
+                      ),
+                    ),
             ],
           ),
         );
